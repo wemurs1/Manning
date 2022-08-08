fe6c0629c25474db2b6504f4b55046189c23ccee
Stuart Murray <stuart.murray.dev@outlook.com>
2022-08-08 17:32:47 +0100

    move to deliverable

---
+++ milestone1/deliverable/BinaryNode.cs
@@ -0,0 +1,38 @@
+using System.Runtime.CompilerServices;
+
+namespace BinaryNode
+{
+    public class BinaryNode<T>
+    {
+        public T Value { get; set; }
+        public BinaryNode<T>? LeftChild { get; set; }
+        public BinaryNode<T>? RightChild { get; set; }
+
+        public BinaryNode(T value)
+        {
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+
+        public void AddLeft(BinaryNode<T> leftChild)
+        {
+            LeftChild = leftChild;
+        }
+
+        public void AddRight(BinaryNode<T> rightChild)
+        {
+            RightChild = rightChild;
+        }
+
+        public override string ToString()
+        {
+            var rootString = Value!.ToString();
+            var leftString = "<null>";
+            var rightString = "<null";
+            if (LeftChild != null && LeftChild.Value != null) leftString = LeftChild.Value.ToString();
+            if (RightChild != null && RightChild.Value != null) rightString = RightChild.Value.ToString();
+            return $"{rootString}: {leftString} {rightString}";
+        }
+    }
+}

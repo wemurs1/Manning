691fffe6592d8bb4bb2b42ca5832763e493c23de
Stuart Murray <stuart.murray.dev@outlook.com>
2022-08-08 15:47:57 +0100

    checkpoint

---
+++ milestone2/BinaryNode/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BinaryNode;
+
+Console.WriteLine("Build Test Tree");
+var root = new BinaryNode<string>("Root");
+var a = new BinaryNode<string>("A");
+var b = new BinaryNode<string>("B");
+var c = new BinaryNode<string>("C");
+var d = new BinaryNode<string>("D");
+var e = new BinaryNode<string>("E");
+var f = new BinaryNode<string>("F");
+
+root.AddLeft(a);
+root.AddRight(b);
+a.AddLeft(c);
+a.AddRight(d);
+b.AddRight(e);
+e.AddLeft(f);
+
+Console.WriteLine("Tree Output");
+Console.WriteLine(root.ToString());
+Console.WriteLine(a.ToString());
+Console.WriteLine(b.ToString());
+Console.WriteLine(c.ToString());
+Console.WriteLine(d.ToString());
+Console.WriteLine(e.ToString());
+Console.WriteLine(f.ToString());
---
+++ milestone2/NaryNode/Program.cs
@@ -0,0 +1,35 @@
+﻿using NaryNode;
+
+Console.WriteLine("Build Test Tree");
+var root = new NaryNode<string>("Root");
+var a = new NaryNode<string>("A");
+var b = new NaryNode<string>("B");
+var c = new NaryNode<string>("C");
+var d = new NaryNode<string>("D");
+var e = new NaryNode<string>("E");
+var f = new NaryNode<string>("F");
+var g = new NaryNode<string>("G");
+var h = new NaryNode<string>("H");
+var i = new NaryNode<string>("I");
+
+root.AddChild(a);
+root.AddChild(b);
+root.AddChild(c);
+a.AddChild(d);
+a.AddChild(e);
+d.AddChild(g);
+c.AddChild(f);
+f.AddChild(h);
+f.AddChild(i);
+
+Console.WriteLine("Tree Output");
+Console.WriteLine(root.ToString());
+Console.WriteLine(a.ToString());
+Console.WriteLine(b.ToString());
+Console.WriteLine(c.ToString());
+Console.WriteLine(d.ToString());
+Console.WriteLine(e.ToString());
+Console.WriteLine(f.ToString());
+Console.WriteLine(g.ToString());
+Console.WriteLine(h.ToString());
+Console.WriteLine(i.ToString());

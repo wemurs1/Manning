bdb2d4dc18a273676ec8bdc5d025766c178595c9
Stuart Murray <stuart.murray.dev@outlook.com>
2022-08-09 09:05:40 +0100

    scaffold milestone 3

---
+++ milestone3/BinaryNode/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BinaryNode;
+
+var root = new BinaryNode<string>("Root");
+var a = new BinaryNode<string>("A");
+var b = new BinaryNode<string>("B");
+var c = new BinaryNode<string>("C");
+var d = new BinaryNode<string>("D");
+var e = new BinaryNode<string>("E");
+var f = new BinaryNode<string>("F");
+
+root.AddLeft(a);
+root.AddRight(b);
+a.AddLeft(c);
+a.AddRight(d);
+b.AddRight(e);
+e.AddLeft(f);
+
+Console.WriteLine(root);
+Console.WriteLine(a);
---
+++ milestone3/NaryNode/Program.cs
@@ -0,0 +1,25 @@
+﻿using NaryNode;
+
+var root = new NaryNode<string>("Root");
+var a = new NaryNode<string>("A");
+var b = new NaryNode<string>("B");
+var c = new NaryNode<string>("C");
+var d = new NaryNode<string>("D");
+var e = new NaryNode<string>("E");
+var f = new NaryNode<string>("F");
+var g = new NaryNode<string>("G");
+var h = new NaryNode<string>("H");
+var i = new NaryNode<string>("I");
+
+root.AddChild(a);
+root.AddChild(b);
+root.AddChild(c);
+a.AddChild(d);
+a.AddChild(e);
+d.AddChild(g);
+c.AddChild(f);
+f.AddChild(h);
+f.AddChild(i);
+
+Console.WriteLine(root);
+Console.WriteLine(a);
